f95f813e8284aaf1455a8e1c39eed6bae6b14e39
Nguyễn Duy Khánh <khanh@MacBook-Air-cua-Nguyen.local>
2023-05-16 21:41:34 +0700

    add

---
+++ main.ts
@@ -0,0 +1,39 @@
+import {handle} from "./src/handle";
+
+const http = require('http')
+const fs = require('fs');
+const PORT: number = 8080;
+export let dataInput = ''
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET') {
+        fs.readFile('./views/input.html', "utf-8", (err, data) => {
+            if (err) {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                return res.end('404 NOT FOUND');
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                return res.end();
+            }
+        })
+    } else {
+        req.on('data', chunk => {
+            dataInput += chunk;
+        });
+        req.on('end', () => {
+        fs.readFile('./views/input.html', "utf-8", (err, data) => {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                data = data.replace("none", handle.zeroToTen)
+                res.write(data);
+                return res.end();
+            });
+        });
+        req.on('error', () => {
+            return res.end('error')
+        });
+    }
+});
+server.listen(PORT, 'localhost', () => {
+    console.log('server is running')
+})
